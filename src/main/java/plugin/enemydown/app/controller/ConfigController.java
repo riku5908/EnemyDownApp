777625fb35721423682372aa14203ecad045f544
riku <rikuri59@gmail.com>
2025-01-31 18:57:03 +0900

    mapperの追加

--- src/main/java/plugin/enemydown/app/controller/ConfigController.java
+++ src/main/java/plugin/enemydown/app/controller/ConfigController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import plugin.enemydown.app.mapper.data.GameConfig;
+import plugin.enemydown.app.mapper.data.SpawnEnemy;
 import plugin.enemydown.app.service.ConfigService;
 
 @RestController
@@ -17,11 +18,16 @@ public class ConfigController {
 
   @RequestMapping(value = "/configList", method = RequestMethod.GET)
   public List<GameConfig> configList() {
-    return service.searchConfig();
+    return service.searchConfigList();
   }
 
   @RequestMapping(value = "/config", method = RequestMethod.GET)
   public GameConfig config(@RequestParam String difficulty) {
     return service.searchConfig(difficulty);
+  }
+
+  @RequestMapping(value = "/spawnEnemyList", method = RequestMethod.GET)
+  public List<SpawnEnemy> spawnEnemyList(@RequestParam String difficulty) {
+    return service.searchSpawnEnemyList(difficulty);
   }
 }
